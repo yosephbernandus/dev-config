c6e0d808a0856b52691df88784614652c940007c
yosephbernandus <yosephbernandus@gmail.com>
2024-04-09 15:28:22 +0700

    Neotree config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -23,7 +23,16 @@ local plugins = {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  }
 }
 
 local opts = {}
@@ -32,6 +41,7 @@ require("lazy").setup(plugins, opts)
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
 
 local config = require("nvim-treesitter.configs")
 config.setup({
