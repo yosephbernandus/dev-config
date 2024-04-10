84d0bbcc39e137760916e0e07dae85dc260b15f0
yosephbernandus <yosephbernandus@gmail.com>
2024-04-10 16:40:38 +0700

    Python ruff lsp config

--- config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -12,6 +12,7 @@ return {
         ensure_installed = {
           "lua_ls",
           "tsserver",
+          "ruff_lsp"
         }
       })
     end
@@ -22,6 +23,7 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.tsserver.setup({})
+      lspconfig.ruff_lsp.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
