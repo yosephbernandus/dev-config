9383849bc0866896a45151a24e3db08098c03a61
yosephbernandus <yosephbernandus@gmail.com>
2024-04-11 12:43:09 +0700

    null-ls: Formatting and linting

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,8 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "rust", "python" },
+--      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "rust", "python" },
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
     })
