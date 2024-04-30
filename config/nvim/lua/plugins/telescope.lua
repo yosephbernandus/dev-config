1f5160f5f5d5d6fea53e220cb2d403b058e2d603
yosephbernandus <yosephbernandus@gmail.com>
2024-04-30 08:29:33 +0700

    tweak telescope function

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -6,6 +6,12 @@ return {
       local builtin = require("telescope.builtin")
       vim.keymap.set('n', '<C-p>', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+      vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
+      vim.keymap.set('n', '<leader>fi', '<cmd>AdvanceGitSearch<CR>')
+      vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
+      vim.keymap.set('n', '<leader>fw', builtin.grep_string, {})
     end
   },
   {
